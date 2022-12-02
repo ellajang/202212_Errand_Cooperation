595cd64700526a6113c728aa32f30db5ab6a07e5
ellajang <3lla.jang@gmail.com>
2022-12-02 23:12:13 +0900

    #2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import "./Header.module.css";
 import { Link } from "react-router-dom";
 import { BiChevronLeft } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
-function Header() {
+function Header1() {
   const navigate = useNavigate();
   return (
-    <div className="header">
+    <div className="header_mypage">
       <BiChevronLeft
-        className="backpage"
+        className="backpage_mypage"
         onClick={() => {
           navigate(-1);
         }}
       />
       <Link to="/home">
-        <img className="header_logo" src="/assets/logo.png" />
+        <img className="header_logo_mypage" src="/assets/logo.png" />
       </Link>
     </div>
   );
 }
 
-export default Header;
+export default Header1;
