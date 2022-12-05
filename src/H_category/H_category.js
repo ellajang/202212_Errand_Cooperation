c533d5a4a1774ab57dafba3815be15fc2d042ae3
ellajang <3lla.jang@gmail.com>
2022-12-05 22:53:51 +0900

    #last

--- src/H_category/H_category.js
+++ src/H_category/H_category.js
@@ -20,84 +20,25 @@ function H_category() {
   return (
     <>
       <Header />
-      <div className="Ct_title">카테고리</div>
-      <ul className="dep1">
-        <li>
-          <a className="depTitle" href="#">
-            연세플라자
-          </a>
-          <ul className="dep2">
-            <li>&nbsp;</li>
-            <li>
-              <a href="#">
-                <Categorylist Key="1" location="뚜레주르" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="2" location="프린트실" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="3" location="봉마르쉐" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="4" location="호아토스트" />
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a className="depTitle" href="#">
-            학관
-          </a>
-          <ul className="dep2">
-            <li>&nbsp;</li>
-            <li>
-              <a href="#">
-                <Categorylist Key="5" location="cu편의점" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="6" location="AA동아리" />
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a className="depTitle" href="#">
-            창조관
-          </a>
-          <ul className="dep2">
-            <li>&nbsp;</li>
-            <li>
-              <a href="#">
-                <Categorylist Key="7" location="304호" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="8" location="402호" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="9" location="202호" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Categorylist Key="10" location="게시판 앞" />
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <div className="footer">&copy;{new Date().getFullYear()} Errand App</div>
+      <p className="Ct_title">카테고리</p>
+      <div className="Ct">
+        <div className="Ct1">
+          <Categorylist Key="1" location="뚜레주르" />
+        </div>
+        <div className="Ct1">
+          <Categorylist Key="2" location="봉마르쉐" />
+        </div>
+        <div className="Ct1">
+          <Categorylist Key="3" location="프린트실" />
+        </div>
+        <div className="Ct1">
+          <Categorylist Key="4" location="cu편의점" />
+        </div>
+        <div className="Ct1">
+          <Categorylist Key="5" location="호아토스트" />
+        </div>
+      </div>
+      <div className="footer_">&copy;{new Date().getFullYear()} Errand App</div>
     </>
   );
 }
